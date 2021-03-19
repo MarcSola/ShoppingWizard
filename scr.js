80f31ae3791a8baec4d3cf407a2cbd538d7aa8f2
jonCroatanUto <jon.garciaorad.asencor@gmail>
2021-03-19 14:26:16 +0100

    prueva slide

---
+++ scr.js
@@ -0,0 +1,11 @@
+document.getElementById('boton').addEventListener('click',change);
+function change(){
+    X=document.querySelector('div.dos')
+    Y=document.querySelector('div.uno')
+    
+    X.classList='uno'
+    
+    Y.classList='dos'
+    
+
+}
