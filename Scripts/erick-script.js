6927b86c17d374cc0c72c26df6c4b9850dde9fd7
Noiztbander <aventum2@gmail.com>
2021-03-22 17:06:49 +0100

    cosas rosas

--- Scripts/erick-script.js
+++ Scripts/erick-script.js
@@ -1,15 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
 //everything that is inside here will be executed JUST when the html is all charged.
 
-var nav_list = document.querySelector(".erick-nav-list::first-children");
-nav_list.getElementsByTagName("li");
 
-	/*
-	function changeColor (){
-			erick_var.style.color = "red";
-	};
+	var nav_list = document.getElementById("erick-nav-list");
+	var childs = nav_list.getElementsByTagName("li");
 
-	*/
+
+	for(li of  childs){
+		li.addEventListener('click', function(){
+			if(this.classList.contains('active')){
+				this.classList.remove("active");
+			} else {
+				this.classList.add("active");
+			}
+		})
+	}
+
 
 
 
