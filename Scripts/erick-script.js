036f083a747bca20d3669a308199df91cf741346
Noiztbander <aventum2@gmail.com>
2021-03-22 15:58:08 +0100

    fixed erick

--- Scripts/erick-script.js
+++ Scripts/erick-script.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
-
 //everything that is inside here will be executed JUST when the html is all charged.
 
+var nav_list = document.querySelector(".erick-nav-list::first-children");
+nav_list.getElementsByTagName("li");
 
+	/*
+	function changeColor (){
+			erick_var.style.color = "red";
+	};
+
+	*/
 
 
 
