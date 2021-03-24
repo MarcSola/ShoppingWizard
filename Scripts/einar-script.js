38199ac5132dec6f2306484bc4181b14adedae07
imalovelyghost <imalovelyghost13@gmail.com>
2021-03-24 17:04:49 +0100

    All updated in my branch!!!!!!

--- Scripts/einar-script.js
+++ Scripts/einar-script.js
@@ -1,61 +1,61 @@
 /* /////////////////////////// */
- 
+
 /* This function is related with aside imgs */
- 
+
 /* /////////////////////////// */
- 
-/*var imgArray = document.querySelectorAll('section.asideContainerEinar div.asideImgContainerClassEinar input');
+
+var imgArray = document.querySelectorAll('div.asideImgContainerClassEinar input');
 //console.log(imgArray)
 imgArray.forEach(function(moveImages) {
- moveImages.addEventListener('mouseover', hovimg)
- moveImages.addEventListener('mouseleave', no_hovimg)
+    moveImages.addEventListener('mouseover', hovimg)
+    moveImages.addEventListener('mouseleave', no_hovimg)
 });
- 
+
 function no_hovimg(event) {
- option = event.target;
- option.style.transform = '';
- option.style.transition = '0.5s';
- option.style.borderRadius = '';
- option.style.border = '';
+    option = event.target;
+    option.style.transform = '';
+    option.style.transition = '0.5s';
+    option.style.borderRadius = '';
+    option.style.border = '';
 }
- 
+
 function hovimg(event) {
- option = event.target;
- option.style.transform = 'translateY(-2px) translateX(+2px) scale(1.1)';
- option.style.transition = '0.5s';
- option.style.borderRadius = '10px';
- option.style.border = '5px solid rgb(255, 204, 0)';
+    option = event.target;
+    option.style.transform = 'translateY(-2px) translateX(+2px) scale(1.1)';
+    option.style.transition = '0.5s';
+    option.style.borderRadius = '10px';
+    option.style.border = '5px solid rgb(255, 204, 0)';
 }
 //closing of moveImages*/
- 
+
 /* /////////////////////////// */
- 
+
 /* This function is for the menu selector related with bike prices */
- 
+
 /* /////////////////////////// */
- 
+
 // AllOptionprices.forEach(function(showSelectedPrice) {
- 
+
 // //Tendrás que gestionar el evento del select y manualmente hacer un innerHTML para ponerle el value dentro
 // //console.log(showSelectedPrice)
 // /*showSelectedPrice.addEventListener('mouseover', hovprice)
 // showSelectedPrice.addEventListener('mouseleave', no_hovprice)*/
 // showSelectedPrice.addEventListener('click', sele)
- 
+
 // });
- 
+
 var myPriceSelector = document.querySelector(".customSelectEinarDiv");
 console.log(myPriceSelector, "aksjhdlksjd")
 myPriceSelector.addEventListener('click', priceSelectedForCostumer)
- 
+
 function priceSelectedForCostumer(event) {
- 
- var AllOptionprices = document.querySelector(".priceSelectorEinarClass");
- console.log(AllOptionprices.value)
- replaceMainPrice(AllOptionprices.value)
- 
+
+    var AllOptionprices = document.querySelector(".priceSelectorEinarClass");
+    console.log(AllOptionprices.value)
+    replaceMainPrice(AllOptionprices.value)
+
 }
- 
+
 /*function no_hovprice(event) {
  option = event.target;
  option.style.borderRadius = '';
@@ -69,11 +69,11 @@ function hovprice(event) {
  option.style.border = '5px solid rgb(184, 29, 168)';
  
 }*/
- 
+
 function replaceMainPrice(price) {
- document.getElementById("finalPriceEinar").innerHTML = price;
+    document.getElementById("finalPriceEinar").innerHTML = price;
 }
- 
+
 /*
 //closing of showSelectedPrice
 console.log(AllOptionprices)
