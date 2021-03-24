38b663670b4875a28aa767e279ae9c0c988f3678
imalovelyghost <imalovelyghost13@gmail.com>
2021-03-24 14:03:19 +0100

    price css, not finished

--- Scripts/einar-script.js
+++ Scripts/einar-script.js
@@ -1,9 +1,16 @@
+/* /////////////////////////// */
+
+
+/* This function is related with aside imgs   */
+
+
+/* /////////////////////////// */
+
 var imgArray = document.querySelectorAll('section.asideContainerEinar div.asideImgContainerClassEinar input');
 //console.log(imgArray)
 imgArray.forEach(function(moveImages) {
     moveImages.addEventListener('mouseover', hov)
     moveImages.addEventListener('mouseleave', no_hov)
-        // moveImages.addEventListener('click',updateValue);
 
     function no_hov(event) {
         option = event.target;
@@ -11,8 +18,6 @@ imgArray.forEach(function(moveImages) {
         option.style.transition = '0.5s';
         option.style.borderRadius = '';
         option.style.border = '';
-        option.style.animation = '';
-
     }
 
     function hov(event) {
@@ -20,9 +25,9 @@ imgArray.forEach(function(moveImages) {
         option.style.transform = 'translateY(-2px) translateX(+2px) scale(1.1)';
         option.style.transition = '0.5s';
         option.style.borderRadius = '10px';
-        option.style.border = '5px solid rgb(255, 204, 0)'
-        option.style.animation = 'glitch1 2.5s infinite'
+        option.style.border = '5px solid rgb(255, 204, 0)';
     }
+    //closing of moveImages
 });
 
 /* /////////////////////////// */
@@ -33,12 +38,60 @@ imgArray.forEach(function(moveImages) {
 
 /* /////////////////////////// */
 
-var AllOptionprices = document.getElementsByTagName("option");
+
+var AllOptionprices = document.querySelectorAll("#priceSelectorEinar");
 //console.log(AllOptionprices)
+
 AllOptionprices.forEach(function(showSelectedPrice) {
-    showSelectedPrice.addEventListener('click', clickf)
+
+    //Tendrás que gestionar el evento del select y manualmente hacer un innerHTML para ponerle el value dentro
+    console.log(showSelectedPrice)
+    showSelectedPrice.addEventListener('mouseover', hov)
+    showSelectedPrice.addEventListener('mouseleave', no_hov)
+    showNewPrice.addEventListener('click', clickf)
+
+    function no_hov(event) {
+        option = event.target;
+        option.style.borderRadius = '';
+        option.style.border = '';
+
+    }
+
+    function hov(event) {
+        option = event.target;
+        option.style.borderRadius = '10px';
+        option.style.border = '5px solid rgb(184, 29, 168)';
+
+    }
+
+    function clickf() {
+        option = event.target;
+        for (var i = 0; i < AllOptionprices.length; i++) {
+            AllOptionprices[i].onclick = function() {
+                alert(this.innerHTML);
+            }
+        }
+
+    }
+
+    //closing of showSelectedPrice
+});
+var mainPRice = document.getElementsByClassName("finalPriceEinarClass");
+
+mainPRice.forEach(function(showNewPrice) {
+
+    showNewPrice.addEventListener('click', clickf)
+    showNewPrice.addEventListener('click', noclickf)
+
+    function noclickf(event) {
+        option = event.target;
+        option.element.innerHTML = ""
+    }
 
     function clickf(event) {
-        document.getElementById("finalPriceEinar").innerHTML = "change this price";
+        option = event.target;
+        option.element.innerHTML = "Hola"
+
     }
+    //closing of showNewPrice
 });
