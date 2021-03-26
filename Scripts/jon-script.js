7f882a41f7d6f69e8d1ea4e9dff68a344b59a5ce
jonCroatanUto <jon.garciaorad.asencor@gmail>
2021-03-26 09:55:41 +0100

    img finish

--- Scripts/jon-script.js
+++ Scripts/jon-script.js
@@ -1,5 +1,5 @@
 var imagArry=document.querySelectorAll('main.main section.jon_slide div input');
-
+var imagenFinish=document.querySelector('section.finish div.izq img')
 imagArry.forEach(function(elem){
     elem.addEventListener('mouseover',hov)
     elem.addEventListener('mouseleave',no_hov)
@@ -29,6 +29,7 @@ function run(){
     forth.classList='tres';
     five.classList='cuatr';
     document.querySelector('main section.blackgrnd').style.backgroundImage=back;
+    document.setAttribute('section.finish div.izq img').style.backgroundImage=back;
     document.querySelector('main section.blackgrnd').style.opacity='0';
     
     document.querySelector('main section.blackgrnd').style.opacity='1';
@@ -71,11 +72,14 @@ function change(event){
     total_cont.insertBefore(container, total_cont.firstChild);
     frst_picture.classList=container.classList;
     container.classList='uno';
+    //La imagen del slide que recive el click , se coloca como backgroundImg.
+
     document.querySelector('main section.blackgrnd').style.backgroundImage=back;
     document.querySelector('main section.blackgrnd').style.opacity='0';
-    
     document.querySelector('main section.blackgrnd').style.opacity='1';
     
-       
+    //esa misma imagen la mando a la ultima pagina.  
+   
+    imagenFinish.setAttribute("src",imag.getAttribute('src')) 
 
 }
